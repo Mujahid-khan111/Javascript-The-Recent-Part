3f1e9663e4d88404dbc6ee4f18810f05820d9124
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-12-04 01:42:33 +0530

    tegged-tamplate

---
+++ 1-Strings/4-Tagged-Template-Exercise.js
@@ -0,0 +1,10 @@
+function upper(strings, ...value){}
+
+var name = "kyle",
+    twitter = "getify",
+    topic = "js Recent Part";
+
+console.log(
+    `Hello ___ (@___),Welcome to ___!` ===
+    "Hello KYLE (@GETIFY),Welcome to js RECENT PARTS!"
+);
