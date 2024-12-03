89f8e4b8ffe72c06930b1ab9b42c7ffd62872e09
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-12-03 15:44:22 +0530

    Template-String.js

---
+++ 1-Strings/1-Template-String.js
@@ -0,0 +1,47 @@
+// ES 5 => one string concate second string and multiple string use (+) operator and  ("") duble cotation
+// Imperative Program
+var name = "kyle simpson";
+var email = "kyle@gmai.com";
+var title = "teacher";
+// console.log("Welcome to this class! you " + title + " is " + name + ", contact : " + email + ".");
+
+const five = 5;
+const ten = 95;
+// console.log("Hundred is" + (five + ten) + "and \n not" + (2* five + ten) + ".");
+
+
+// ES6
+
+let a = 5;
+let b = 10;
+console.log(`The sum of ${a} and ${b} is ${a + b}.`);
+
+
+// Basic Syntax 
+var name = "mujahid"
+var age = 21;
+
+var message = `hello, My name is ${name} and I am ${age} Years old`;
+// console.log(message);
+
+
+var name = "mujahid khan";
+let Email = "khanmujahid7003@gmail.com";
+var title = "Teacher";
+
+var message = `Welcome to this class! your 
+${title} is ${name}, contact: ${Email}`
+
+console.log(message);
+// Welcome to this class! your 
+// Teacher is mujahid khan, contact: khamujahid7003@gmail.com
+
+
+let n = "Mujahid";
+let ag = "21";
+let ti = "Furnted dowloper Prosessing"
+
+let messages = `Welcome to my website! My name is ${n}
+and  im ${ag} year old and i am ${ti}`
+
+console.log(messages);
